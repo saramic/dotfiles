3601c472f6837e4304a48f5a83722f18fa939ea3
Michael Milewski <saramic@gmail.com>
2017-09-01 06:19:07 +0000

    first go at ruby dev yum file

---
+++ yum_install.sh
@@ -0,0 +1,11 @@
+# update
+sudo yum -y update
+
+# basics for development
+sudo yum -y install readline-devel git make zlib-devel sqlite-devel.x86_64 gcc g++ svn
+
+# ruby native extension compilation
+sudo yum -y install ruby-devel rubygems compass
+
+# tmux
+sudo yum install tmux
